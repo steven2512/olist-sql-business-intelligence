0cc731f568805b36e8f260bdd84e47fcfd5a8851
Nguyen Duong <nguyenduong251202@gmail.com>
2026-03-14 15:24:17 +1100

    Summarized the row counts for all tables with index/partitions considered

--- 01_eda/01_database_exploration.sql
+++ 01_eda/01_database_exploration.sql
@@ -8,3 +8,11 @@ SELECT *
 SELECT COUNT(*) AS TOTAL_TABLES
   FROM INFORMATION_SCHEMA.TABLES
  WHERE TABLE_TYPE = 'BASE TABLE';
+
+-- How many rows does each table have?
+SELECT
+    t.name,
+    SUM(p.rows) AS total_rows
+FROM sys.tables t
+INNER JOIN sys.partitions p
+WHERE p.index_id IN (0,1)
