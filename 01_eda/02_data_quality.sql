d508228b333a704011dd7ca646f041d69e20e27e
Nguyen Duong <nguyenduong251202@gmail.com>
2026-03-18 22:04:19 +1100

    Developed the null counter stored procedure to be much more efficient than the first version

--- 01_eda/02_data_quality.sql
+++ 01_eda/02_data_quality.sql
@@ -1,61 +1,50 @@
 -- 1. How many null values are in all columns of all tables?
-
-DROP PROCEDURE IF EXISTS name_count;
+-- Build a procedure that displays null counts for each column of each table
+DROP PROCEDURE IF EXISTS sp_null_counter;
 GO
 
-CREATE PROCEDURE name_count
-AS
+CREATE PROCEDURE sp_null_counter AS
 BEGIN
-	-- all table names and their columns
+	DROP TABLE IF EXISTS null_count_result;
+	DECLARE @querry NVARCHAR(MAX)
 	SELECT
-		table_name,
-		column_name
-	INTO #table_column
-	FROM information_schema.columns
-
-	-- same table as above but extra column for null counts
-	SELECT
-		table_name,
-		column_name,
-		0 AS total_null_count
-	INTO #null_count
-	FROM information_schema.columns
+		@querry = STRING_AGG(CAST(
+			CONCAT('SELECT ',
+			'''',
+			table_name,
+			'''',
+			' AS table_name', 
+			', ',
+			'''',
+			column_name,
+			'''',
+			' AS column_name',
+			', ',
+			'COUNT(*) AS null_counts FROM ',
+			table_name,
+			' WHERE ',
+			column_name,
+			' IS NULL') AS NVARCHAR(MAX)), ' UNION ALL ')
+	FROM INFORMATION_SCHEMA.columns
+    
+    CREATE TABLE null_count_result (
+        table_name NVARCHAR(100),
+        column_name NVARCHAR(100),
+        null_counts INT
+    )
+    
+    INSERT INTO null_count_result
+	EXEC sp_executesql @querry
 	
-	--extract table_name and column_name line by line
-	--and find total null counts
-	--then update it to #null_count
-	WHILE EXISTS (SELECT * FROM #table_column)
-	BEGIN
-		DECLARE @tbl NVARCHAR(50)
-		DECLARE @col NVARCHAR(50)
-		DECLARE @total_count INT
-		DECLARE @querry NVARCHAR(MAX)
-		
-		SELECT TOP 1
-			@tbl = table_name,
-			@col = column_name
-		FROM #table_column
-
-		SET @querry = 'SELECT @total_count = COUNT(*) FROM ' 
-						+ @tbl + 
-						' WHERE ' + @col + ' IS NULL'
-		EXEC sp_executesql 
-			@querry,
-			N'@total_count INT OUTPUT',
-			@total_count OUTPUT
-
-
-		UPDATE #null_count 
-		SET total_null_count = @total_count
-		WHERE table_name = @tbl AND column_name = @col
-
-		DELETE FROM #table_column
-		WHERE table_name = @tbl AND column_name = @col
-		
-	END
-	
-	SELECT * FROM #null_count;
+	SELECT * FROM null_count_result
 END
 GO
 
-EXEC name_count;
+EXEC sp_null_counter
+
+
+DROP TABLE IF EXISTS null_count_result;
+
+--Approach: build a count NA query string for each table + column combination then use stored procedure sp_executesql to execute that querry in 1 pass
+-- Result: 1 table with table_name, column_name, and null_counts
+
